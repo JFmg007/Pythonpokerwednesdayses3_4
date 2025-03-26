34630bc58deb21aaaa6334500ae8536a3e649652
JFmg007 <jf.manidoni@gmail.com>
2025-03-26 18:40:04 +0100

    first version of the poker game

--- deck.py
+++ deck.py
@@ -1,8 +1,8 @@
 import random
 
 class Card:
-    RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10","J", "Q", "K", "A"]
-    SUITS = ["♠","♥","♦","♣"]
+    RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    SUITS = ["♣", "♦", "♥", "♠"]
     def __init__(self, suit, rank):
         if rank not in self.RANKS:
             raise ValueError("Invalid rank")
@@ -10,6 +10,12 @@ class Card:
             raise ValueError("Invalid suit")
         self._suit = suit
         self._rank = rank
+
+    def __eq__(self, other):
+        return self.rank == other.rank
+
+    def __gt__(self, other):
+        return self.RANKS.index(self.rank) > self.RANKS.index(other.rank)
 
     def __str__(self):
         return f"{self._rank}{self._suit}"
@@ -47,4 +53,3 @@ if __name__ == "__main__":
     deck.shuffle()
     print(deck)
     print(deck.deal())
-
